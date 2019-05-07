4758d9590ac77803bea142948813dfa8f2f19cd1
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-07 23:54:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,6 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-    # set the :foo key of the session hash equal to 'hello' here!
     session[:foo] = 'hello'
     if session[:foo] == 'hello'
       redirect '/fetch'
